70d114ed3360fbe6d93f734bc4ccb57b78976ac2
sdeabhi <abhinandan.prajapati@kindasolutions.com>
2025-11-14 16:22:39 +0530

    updated

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,21 +4,21 @@ const Contact: React.FC = () => {
   return (
     <div className="p-5 font-sans flex flex-col md:flex-row md:justify-between">
       <p className="mb-2 md:mb-0">
-        <a href="mailto:sdeabhinandan@gmail.com" className="text-gray-500 no-underline">
+        <a href="mailto:sdeabhinandan@gmail.com" className="text-gray-100 no-underline bg-black rounded-md p-2">
           sdeabhinandan@gmail.com
         </a>
       </p>
       <p className="mb-2 md:mb-0">
-        <a href="tel:+917408331278" className="text-gray-500 no-underline">
-          7408331278
+        <a href="tel:+917408331278" className="text-gray-100 no-underline bg-black rounded-md p-2">
+          +91 7408331278
         </a>
       </p>
       <p>
         <a
-          href="https://drive.google.com/file/d/1nHx4u4vx7JEMFfm0P_ZfeGeLuAKPzHVe/view?usp=sharing"
+          href="https://drive.google.com/file/d/1_-HI5734jp0eAp4UeZOiPUlDdTxAhhxm/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-500 no-underline"
+          className="text-gray-100 no-underline bg-black rounded-md p-2"
         >
           Download Resume
         </a>
